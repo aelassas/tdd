7dc5a04279a83411dc884257edc7801d5202798c
aelassas <akram.elassas@gmail.com>
2023-12-11 19:35:07 +0100

    Update DictionaryTest.cs

--- tests/Dictionary.IntegrationTests/DictionaryTest.cs
+++ tests/Dictionary.IntegrationTests/DictionaryTest.cs
@@ -1,41 +1,39 @@
-namespace Dictionary.IntegrationTests
+namespace Dictionary.IntegrationTests;
+
+public class DictionaryTest
 {
-    public class DictionaryTest
+    [Fact]
+    public void TestDictionaryName()
     {
-        [Fact]
-        public void TestDictionaryName()
-        {
-            var dictionary = new Dictionary(new DictionaryParser(new DictionaryLoader(@"..\..\..\..\..\data\dict-name.txt")));
-            Assert.Equal("en-fr", dictionary.Name);
-        }
+        var dictionary = new Dictionary(new DictionaryParser(new DictionaryLoader(@"..\..\..\..\..\data\dict-name.txt")));
+        Assert.Equal("en-fr", dictionary.Name);
+    }
 
-        [Fact]
-        public void TestIsEmpty()
-        {
-            var dictionary = new Dictionary(new DictionaryParser(new DictionaryLoader(@"..\..\..\..\..\data\dict-name.txt")));
-            Assert.True(dictionary.IsEmpty());
-        }
+    [Fact]
+    public void TestIsEmpty()
+    {
+        var dictionary = new Dictionary(new DictionaryParser(new DictionaryLoader(@"..\..\..\..\..\data\dict-name.txt")));
+        Assert.True(dictionary.IsEmpty());
+    }
 
-        [Fact]
-        public void TestIsNotEmpty()
-        {
-            var dictionary = new Dictionary(new DictionaryParser(new DictionaryLoader(@"..\..\..\..\..\data\dict.txt")));
-            Assert.False(dictionary.IsEmpty());
-        }
+    [Fact]
+    public void TestIsNotEmpty()
+    {
+        var dictionary = new Dictionary(new DictionaryParser(new DictionaryLoader(@"..\..\..\..\..\data\dict.txt")));
+        Assert.False(dictionary.IsEmpty());
+    }
 
-        [Fact]
-        public void TestOneTranslation()
-        {
-            var dictionary = new Dictionary(new DictionaryParser(new DictionaryLoader(@"..\..\..\..\..\data\dict.txt")));
-            Assert.Equal("contre", dictionary.GetTranslation("against")[0]);
-        }
+    [Fact]
+    public void TestOneTranslation()
+    {
+        var dictionary = new Dictionary(new DictionaryParser(new DictionaryLoader(@"..\..\..\..\..\data\dict.txt")));
+        Assert.Equal("contre", dictionary.GetTranslation("against")[0]);
+    }
 
-        [Fact]
-        public void TestMultipleTranslations()
-        {
-            var dictionary = new Dictionary(new DictionaryParser(new DictionaryLoader(@"..\..\..\..\..\data\dict.txt")));
-            Assert.Equal(new[] { "contre", "versus" }, dictionary.GetTranslation("against"));
-        }
-
+    [Fact]
+    public void TestMultipleTranslations()
+    {
+        var dictionary = new Dictionary(new DictionaryParser(new DictionaryLoader(@"..\..\..\..\..\data\dict.txt")));
+        Assert.Equal(new[] { "contre", "versus" }, dictionary.GetTranslation("against"));
     }
 }
